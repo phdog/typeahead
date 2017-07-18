c61adc1cf77d110a36f70d934de511b8256b8657
Качанов Максим <max.kachanov@gmail.com>
2017-07-18 11:52:33 +0300

    fixed bug with invalid input

--- client/search/components/Input.tsx
+++ client/search/components/Input.tsx
@@ -51,7 +51,7 @@ class SearchTextInput extends React.Component<IState & DispatchProps, void> {
       switch(e.keyCode) {
         case 13: //Enter
           e.preventDefault();
-          if (active.key) {
+          if (active && active.key) {
             browserHistory.push(active.key);
             pickSearch(active.key)
             stopSearch();
